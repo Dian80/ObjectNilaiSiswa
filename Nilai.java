080aa35998267b2e9e2b7df74badcecad5b46386
Dian80 <70132205+Dian80@users.noreply.github.com>
2020-11-09 21:11:13 +0700

    Add files via upload

---
+++ Nilai.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programnilai;
+
+/**
+ *
+ * @author Adi Arput
+ */
+public class Nilai {
+    public int getnilaiPengetahuan (int pengetahuan){
+        return pengetahuan;
+    }
+    public int getnilaiKeterampilan (int keterampilan){
+        return keterampilan;
+    }
+}
